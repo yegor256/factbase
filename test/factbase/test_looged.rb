347ae813ab618878fda745d8417bf8e8dd476aaa
Yegor Bugayenko <yegor256@gmail.com>
2024-05-15 14:35:28 -0400

    #26 tests

--- test/factbase/test_looged.rb
+++ test/factbase/test_looged.rb
@@ -43,4 +43,8 @@ class TestLooged < Minitest::Test
     assert_equal(2, found)
     assert_equal(2, fb.size)
   end
+
+  def test_returns_int
+    assert_equal(0, Factbase::Looged.new(Factbase.new, Loog::NULL).query('()').each(&:to_s))
+  end
 end
