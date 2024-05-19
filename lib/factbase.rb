c9c2ae86f8b2ac12509499171f9a4e18f0a73476
rultor <me@rultor.com>
2024-05-19 09:34:58 +0000

    version set to 0.0.27

--- lib/factbase.rb
+++ lib/factbase.rb
@@ -29,7 +29,7 @@ require 'yaml'
 # License:: MIT
 class Factbase
   # Current version of the gem (changed by .rultor.yml on every release)
-  VERSION = '0.0.0'
+  VERSION = '0.0.27'
 
   # Constructor.
   def initialize(facts = [])
