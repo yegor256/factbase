e42efb73675df9932549488876cce72d0101cd58
Yegor Bugayenko <yegor256@gmail.com>
2024-06-03 16:09:50 +0300

    #59 check re

--- lib/factbase/term.rb
+++ lib/factbase/term.rb
@@ -306,6 +306,7 @@ class Factbase::Term
     raise "A symbol expected as first argument of 'defn'" unless fn.is_a?(Symbol)
     raise "Can't use '#{fn}' name as a term" if Factbase::Term.instance_methods(true).include?(fn)
     raise "Term '#{fn}' is already defined" if Factbase::Term.private_instance_methods(false).include?(fn)
+    raise "The '#{fn}' is a bad name for a term" unless fn.match?(/^[a-z_]+$/)
     e = "class Factbase::Term\nprivate\ndef #{fn}(fact, maps)\n#{@operands[1]}\nend\nend"
     # rubocop:disable Security/Eval
     eval(e)
