3c9bb6021669c90900e469c1c1bf0b2423561748
Yegor Bugayenko <yegor256@gmail.com>
2024-07-23 05:53:58 +0300

    #100: #102 fixed and tested

--- test/factbase/terms/test_system.rb
+++ test/factbase/terms/test_system.rb
@@ -34,4 +34,10 @@ class TestSystem < Minitest::Test
     t = Factbase::Term.new(:env, ['FOO', ''])
     assert_equal('bar', t.evaluate(fact, []))
   end
+
+  def test_default
+    ENV.delete('FOO')
+    t = Factbase::Term.new(:env, ['FOO', 'мой друг'])
+    assert_equal('мой друг', t.evaluate(fact, []))
+  end
 end
