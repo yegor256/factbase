65652ae013d64550d08b19b092ebc8ade225bba4
Yegor Bugayenko <yegor256@gmail.com>
2025-01-27 14:31:45 +0300

    join

--- benchmarks/simple.rb
+++ benchmarks/simple.rb
@@ -28,7 +28,7 @@ require_relative '../lib/factbase'
 
 QUERY_RUNS = 10
 TRANSACTION_RUNS = 1_000
-INSERTION_COUNT = 1000
+INSERTION_COUNT = 100
 
 sum = {}
 
@@ -54,7 +54,8 @@ queries = [
   '(eq title \'Object Thinking 5000\')',
   '(gt time \'2024-03-23T03:21:43Z\')',
   '(and (eq foo 42.998) (or (gt bar 200) (absent zzz)))',
-  '(eq id (agg (always) (max id)))'
+  '(eq id (agg (always) (max id)))',
+  '(join "c<=cost,b<=bar" (eq id (agg (always) (max id))))'
 ]
 
 queries.each do |q|
