ed983c40935afaa77d41a8dd8e74b83caf5c8919
Yegor Bugayenko <yegor256@gmail.com>
2025-03-07 13:40:20 +0300

    #192: doc

--- lib/factbase.rb
+++ lib/factbase.rb
@@ -50,6 +50,24 @@ require 'yaml'
 #  fb2 = Factbase.new # it's empty
 #  fb2.import(File.binread(file))
 #
+# Here's how to use transactions to ensure data consistency:
+#
+#  fb = Factbase.new
+#  # Successful transaction
+#  fb.txn do |fbt|
+#    f = fbt.insert
+#    f.name = 'John'
+#    f.age = 30
+#    # If any error occurs here, all changes will be rolled back
+#  end
+#  # Transaction with rollback
+#  fb.txn do |fbt|
+#    f = fbt.insert
+#    f.name = 'Jane'
+#    f.age = 25
+#    raise Factbase::Rollback # This will undo all changes in this transaction
+#  end
+#
 # It's impossible to delete properties of a fact. It is however possible to
 # delete the entire fact, with the help of the +query()+ and then +delete!()+
 # methods.
