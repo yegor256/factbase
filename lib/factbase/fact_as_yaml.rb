7080a7e840a7edc2bde28b3c6d6f029f362f55f8
Yegor Bugayenko <yegor256@gmail.com>
2025-06-03 13:02:00 +0300

    #235: Add FactAsYaml class for YAML conversion.

---
+++ lib/factbase/fact_as_yaml.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+# SPDX-FileCopyrightText: Copyright (c) 2024-2025 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
+require 'yaml'
+require_relative '../factbase'
+
+# Single fact to YAML converter.
+#
+# This class helps converting a single fact to YAML format, for example:
+#
+#  require 'factbase/fact_as_yaml'
+#  fb = Factbase.new
+#  f = fb.query('(eq foo 42)').to_a.first
+#  puts Factbase::FactAsYaml.new(f).to_s
+#
+# Author:: Yegor Bugayenko (yegor256@gmail.com)
+# Copyright:: Copyright (c) 2024-2025 Yegor Bugayenko
+# License:: MIT
+class Factbase::FactAsYaml
+  # Constructor.
+  def initialize(fact)
+    @fact = fact
+  end
+
+  # Convert the fact to YAML.
+  # @return [String] The fact in YAML format
+  def to_s
+    props = @fact.all_properties
+    hash = {}
+    props.each do |p|
+      v = @fact[p]
+      hash[p] = v.is_a?(Array) ? v : [v]
+    end
+    hash.sort.to_h.to_yaml
+  end
+end
