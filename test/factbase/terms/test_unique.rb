79b1026877139269107bfb4d432c45fa5c7df107
volodya-lombrozo <volodya.lombrozo@gmail.com>
2025-10-17 17:30:10 +0300

    feat(#302): Add Env term for evaluating environment variables

--- test/factbase/terms/test_unique.rb
+++ test/factbase/terms/test_unique.rb
@@ -3,15 +3,14 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024-2025 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
-# Test for unique term.
-# Author:: Volodya Lombrozo (volodya.lombrozo@gmail.com)
-# Copyright:: Copyright (c) 2024-2025 Yegor Bugayenko
-# License:: MIT
-
 require_relative '../../test__helper'
 require_relative '../../../lib/factbase/term'
 require_relative '../../../lib/factbase/terms/unique'
 
+# Test for unique term.
+# Author:: Volodya Lombrozo (volodya.lombrozo@gmail.com)
+# Copyright:: Copyright (c) 2024-2025 Yegor Bugayenko
+# License:: MIT
 class TestUnique < Factbase::Test
   def test_unique
     t = Factbase::Unique.new([:foo])
