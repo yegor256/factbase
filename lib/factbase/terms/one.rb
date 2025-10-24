3edb537121b9a3ad3f8ec176d5dd74cf2e0532c6
volodya-lombrozo <volodya.lombrozo@gmail.com>
2025-10-24 13:18:35 +0300

    feat(#302): Add 'one' term functionality and remove 'meta' term

---
+++ lib/factbase/terms/one.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# SPDX-FileCopyrightText: Copyright (c) 2024-2025 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
+require_relative 'base'
+
+# The term which property has exactly one value.
+# Author:: Yegor Bugayenko (yegor256@gmail.com)
+# Copyright:: Copyright (c) 2024-2025 Yegor Bugayenko
+# License:: MIT
+class Factbase::One < Factbase::TermBase
+  # Constructor.
+  # @param [Array] operands Operands
+  def initialize(operands)
+    super()
+    @operands = operands
+  end
+
+  # Evaluate term on a fact.
+  # @param [Factbase::Fact] fact The fact
+  # @param [Array<Factbase::Fact>] maps All maps available
+  # @param [Factbase] fb Factbase to use for sub-queries
+  # @return [Boolean] True if exactly one value
+  def evaluate(fact, maps, fb)
+    assert_args(1)
+    v = _values(0, fact, maps, fb)
+    !v.nil? && v.size == 1
+  end
+end
