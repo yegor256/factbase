31029c2a48342cea47390955c126ff3efc520bd5
maxonfjvipon <mtrunnikov@gmail.com>
2025-11-11 21:54:20 +0300

    bug(#363): file

---
+++ lib/factbase/indexed/indexed_exists.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# SPDX-FileCopyrightText: Copyright (c) 2024-2025 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
+# Indexed term 'exists'.
+class Factbase::IndexedExists
+  def initialize(term, idx)
+    @term = term
+    @idx = idx
+  end
+
+  def predict(maps, _fb, _params)
+    return nil if @idx.nil?
+    key = [maps.object_id, @term.operands.first, @term.op]
+    if @idx[key].nil?
+      @idx[key] = []
+      prop = @operands.first.to_s
+      maps.to_a.each do |m|
+        @idx[key].append(m) unless m[prop].nil?
+      end
+    end
+    (maps & []) | @idx[key]
+  end
+end
