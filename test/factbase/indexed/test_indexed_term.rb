5a0a46744e6182bd0ffe85b247b1e61ec840b9ee
maxonfjvipon <mtrunnikov@gmail.com>
2025-11-12 16:55:03 +0300

    bug(#363): todo

--- test/factbase/indexed/test_indexed_term.rb
+++ test/factbase/indexed/test_indexed_term.rb
@@ -126,6 +126,9 @@ class TestIndexedTerm < Factbase::Test
     assert_kind_of(Factbase::Taped, n)
   end
 
+  # @todo #363:30min Move the test to a separated class. Since we've moved prediction of 'unqieu' term
+  #  to separated class IndexedUnique, let's move this test to a separated test class too in order to be
+  #  consistent with rule 'one class - one test class'
   def test_predicts_on_unique
     term = Factbase::Term.new(:unique, [:foo])
     idx = {}
