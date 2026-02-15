b12fc9d87344731d554fba78a4d6f60639896acb
Philip Belousov <belousovfilip@gmail.com>
2026-02-15 23:50:42 +0300

    #506: Fixed IndexedOne to preserve identity and transaction state

--- test/factbase/indexed/test_indexed_factbase.rb
+++ test/factbase/indexed/test_indexed_factbase.rb
@@ -308,4 +308,28 @@ class TestIndexedFactbase < Factbase::Test
     end
     assert_equal(2, fb.query('(eq scope 1)').each.to_a.size)
   end
+
+  def test_term_one_keeps_duplicates
+    fb = Factbase.new
+    fb.insert.scope = 10
+    fb.insert.scope = 10
+    assert_equal(2, fb.query('(one scope)').each.to_a.size)
+  end
+
+  def test_indexed_term_one_keeps_duplicates
+    fb = Factbase::IndexedFactbase.new(Factbase.new)
+    fb.insert.scope = 10
+    fb.insert.scope = 10
+    assert_equal(2, fb.query('(one scope)').each.to_a.size)
+  end
+
+  def test_indexed_term_one_keeps_duplicates_in_txn
+    fb = Factbase::IndexedFactbase.new(Factbase.new)
+    fb.txn do |fbt|
+      fbt.insert.scope = 10
+      fbt.insert.scope = 10
+      assert_equal(2, fbt.query('(one scope)').each.to_a.size)
+    end
+    assert_equal(2, fb.query('(one scope)').each.to_a.size)
+  end
 end
--- test/factbase/indexed/test_indexed_one.rb
+++ test/factbase/indexed/test_indexed_one.rb
@@ -7,6 +7,7 @@ require_relative '../../test__helper'
 require_relative '../../../lib/factbase'
 require_relative '../../../lib/factbase/term'
 require_relative '../../../lib/factbase/taped'
+require_relative '../../../lib/factbase/lazy_taped'
 require_relative '../../../lib/factbase/indexed/indexed_term'
 require_relative '../../../lib/factbase/indexed/indexed_one'
 
@@ -15,13 +16,46 @@ require_relative '../../../lib/factbase/indexed/indexed_one'
 # Copyright:: Copyright (c) 2024-2026 Yegor Bugayenko
 # License:: MIT
 class TestIndexedOne < Factbase::Test
-  def test_predicts_on_one
-    term = Factbase::Term.new(:one, [:foo])
-    idx = {}
-    term.redress!(Factbase::IndexedTerm, idx:)
-    maps = Factbase::Taped.new([{ 'foo' => [42] }, { 'bar' => [7] }, { 'foo' => [22, 42] }, { 'foo' => [] }])
-    n = term.predict(maps, nil, {})
-    assert_equal(1, n.size)
-    assert_kind_of(Factbase::Taped, n)
+  def test_predicts_on_one_with_array
+    _assert_one { |input| input }
+  end
+
+  def test_predicts_on_one_with_taped
+    _assert_one { |input| Factbase::Taped.new(input) }
+  end
+
+  def test_predicts_on_one_with_lazy_taped
+    _assert_one { |input| Factbase::LazyTaped.new(input) }
+  end
+
+  def test_predict_decorator_persistence
+    [
+      { input: [{ 'foo' => [42] }], expected: Array },
+      { input: Factbase::Taped.new([{ 'foo' => [42] }]), expected: Factbase::Taped },
+      { input: Factbase::LazyTaped.new([{ 'foo' => [42] }]), expected: Factbase::Taped }
+    ].each do |c|
+      term = Factbase::Term.new(:one, [:foo])
+      term.redress!(Factbase::IndexedTerm, idx: {})
+      n = term.predict(c[:input], nil, {})
+      assert_kind_of(c[:expected], n, "Failed persistence for #{c[:input].class}")
+    end
+  end
+
+  private
+
+  def _assert_one
+    [
+      { input: [{ 'foo' => [42] }, { 'bar' => [1] }], expected: 1 },
+      { input: [{ 'foo' => [42, 43] }], expected: 0 },
+      { input: [{ 'bar' => [1] }], expected: 0 },
+      { input: [{ 'foo' => [1] }, { 'foo' => [1, 2] }, { 'foo' => [3] }], expected: 2 }
+    ].each do |c|
+      idx = {}
+      maps = yield(c[:input])
+      term = Factbase::Term.new(:one, [:foo])
+      term.redress!(Factbase::IndexedTerm, idx: idx)
+      n = term.predict(maps, nil, {})
+      assert_equal(c[:expected], n.size, "Failed for #{maps.class} with #{c[:input]}")
+    end
   end
 end
