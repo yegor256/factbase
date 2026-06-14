525344f00f468f551dd22171968ab6b472edaf23
Vasilev Stas <33578155+VasilevNStas@users.noreply.github.com>
2026-06-14 19:47:06 +0300

    #629: Add formatter tests for ToJSON, ToYAML, ToXML (#626)
    * Add formatter tests for ToJSON, ToYAML, ToXML
    
    Added tests for empty factbase, time values, string values,
    and custom sort key (JSON).
    
    * #629: fix RuboCop Lint/NumberConversion and yamllint

--- test/factbase/test_to_json.rb
+++ test/factbase/test_to_json.rb
@@ -30,4 +30,33 @@ class TestToJSON < Factbase::Test
     json = Factbase::ToJSON.new(fb).json
     assert_includes(json, '{"a":256,"b":1,"c":42}', json)
   end
+
+  def test_empty_factbase
+    assert_equal('[]', Factbase::ToJSON.new(Factbase.new).json)
+  end
+
+  def test_time_value
+    fb = Factbase.new
+    fb.insert.when = Time.now
+    json = JSON.parse(Factbase::ToJSON.new(fb).json)
+    assert_kind_of(String, json[0]['when'])
+    refute_empty(json[0]['when'])
+  end
+
+  def test_string_value
+    fb = Factbase.new
+    fb.insert.text = 'hello'
+    assert_equal('hello', JSON.parse(Factbase::ToJSON.new(fb).json)[0]['text'])
+  end
+
+  def test_custom_sort_key
+    fb = Factbase.new
+    fb.insert.prio = 2
+    fb.insert.prio = 1
+    fb.insert.prio = 3
+    json = JSON.parse(Factbase::ToJSON.new(fb, 'prio').json)
+    assert_equal(1, json[0]['prio'])
+    assert_equal(2, json[1]['prio'])
+    assert_equal(3, json[2]['prio'])
+  end
 end
