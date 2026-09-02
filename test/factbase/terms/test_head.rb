3d900ba1df2d508c961e78de7d3fe787b822d36b
devil <europedreadlydevil@gmail.com>
2026-09-02 09:21:22 +0000

    #702: satisfy rubocop in the new test

--- test/factbase/terms/test_head.rb
+++ test/factbase/terms/test_head.rb
@@ -3,9 +3,9 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024-2026 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
-require_relative '../../test__helper'
 require_relative '../../../lib/factbase/term'
 require_relative '../../../lib/factbase/terms/head'
+require_relative '../../test__helper'
 
 # Test for head term.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
@@ -14,8 +14,10 @@ require_relative '../../../lib/factbase/terms/head'
 class TestHead < Factbase::Test
   def test_takes_the_first_ones
     maps = [{ 'foo' => [1] }, { 'foo' => [2] }, { 'foo' => [3] }]
-    t = Factbase::Term.new(:head, [2, Factbase::Term.new(:always, [])])
-    assert_equal(2, t.predict(maps, Factbase.new(maps), {}).size)
+    assert_equal(
+      2,
+      Factbase::Term.new(:head, [2, Factbase::Term.new(:always, [])]).predict(maps, Factbase.new(maps), {}).size
+    )
   end
 
   def test_refuses_a_negative_count
