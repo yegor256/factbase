3124f863da708fe9cecc64eb841cc0c14dd2201a
morphqdd <europeDreadlyDevil@gmail.com>
2026-09-04 09:11:37 +0300

    #711: leave a non-term operand alone while simplifying (#733)

--- lib/factbase/terms/simplified.rb
+++ lib/factbase/terms/simplified.rb
@@ -20,7 +20,7 @@ class Factbase::Simplified
     strs = []
     ops = []
     @operands.each do |o|
-      o = o.simplify
+      o = o.simplify if o.is_a?(Factbase::Term)
       s = o.to_s
       next if strs.include?(s)
       strs << s
