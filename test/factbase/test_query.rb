39d8179ba55b67cff0019d54ee88fde16ce55689
morphqdd <europeDreadlyDevil@gmail.com>
2026-09-04 09:11:51 +0300

    #709: keep a fact whose property carries a new value (#730)
    * #709: keep a fact whose property carries a new value
    
    * #709: update the count the new rule gives

--- test/factbase/test_query.rb
+++ test/factbase/test_query.rb
@@ -100,7 +100,7 @@ class TestQuery < Factbase::Test
       '(eq "Integer" (type hi))' => 1,
       '(when (eq num 0) (exists time))' => 2,
       '(when (unique num) (or (eq num 0) (eq num 42)))' => 3,
-      '(unique num)' => 1,
+      '(unique num)' => 2,
       '(unique name)' => 2,
       '(unique pi)' => 1,
       '(many num)' => 1,
