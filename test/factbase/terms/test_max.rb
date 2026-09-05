58479f67d0412341dd0afbe90829aeba8ae6b5af
devil <europedreadlydevil@gmail.com>
2026-09-05 06:47:24 +0000

    #747: report which two values cannot be compared

--- test/factbase/terms/test_max.rb
+++ test/factbase/terms/test_max.rb
@@ -38,4 +38,14 @@ class TestMax < Factbase::Test
       )
     )
   end
+
+  def test_reports_incomparable_values
+    fb = Factbase.new
+    fb.insert.v = 's'
+    fb.insert.v = 1
+    ['(gt (max v) 0)', '(gt (min v) 0)'].each do |q|
+      e = assert_raises(StandardError, q) { fb.query(q).each.to_a }
+      assert_includes(e.message, "Can't compare '1' (Integer) with 's' (String) in the 'v' property", q)
+    end
+  end
 end
