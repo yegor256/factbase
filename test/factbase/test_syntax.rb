1eb1396d3b965f2cee82b70975fefa9d4cafe29b
devil <europedreadlydevil@gmail.com>
2026-09-07 09:08:50 +0000

    #767: accept a float the way Ruby writes it

--- test/factbase/test_syntax.rb
+++ test/factbase/test_syntax.rb
@@ -108,6 +108,15 @@ class TestSyntax < Factbase::Test
     )
   end
 
+  def test_parses_floats_the_way_ruby_writes_them
+    ['1.5e10', '1.5E10', '1e10', '1E+10', '1.5e+10', '2e-3'].each do |n|
+      assert(
+        Factbase::Syntax.new("(eq t #{n})").to_term.evaluate({ 't' => Float(n) }, [], Factbase.new),
+        n
+      )
+    end
+  end
+
   def test_simple_matching
     m = { 'foo' => ['Hello, world!'], 'bar' => [42], 'z' => [1, 2, 3, 4] }
     {
