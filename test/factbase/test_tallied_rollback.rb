651d11529df28dd32ec64c6a362ebddd64c8ea0c
devil <europedreadlydevil@gmail.com>
2026-09-09 10:06:14 +0000

    #833: sort the requires

--- test/factbase/test_tallied_rollback.rb
+++ test/factbase/test_tallied_rollback.rb
@@ -3,9 +3,9 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024-2026 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
-require_relative '../test__helper'
 require_relative '../../lib/factbase'
 require_relative '../../lib/factbase/tallied'
+require_relative '../test__helper'
 
 # Test.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
