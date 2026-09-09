1de4307a97fc870d1790c1deb8cf65c6782a714f
devil <europedreadlydevil@gmail.com>
2026-09-09 09:48:08 +0000

    #831: test that a negative position is refused

---
+++ test/factbase/test_term_at.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
+require_relative '../test__helper'
+require_relative '../../lib/factbase'
+
+# Test.
+# Author:: Yegor Bugayenko (yegor256@gmail.com)
+# Copyright:: Copyright (c) 2024-2026 Yegor Bugayenko
+# License:: MIT
+class TestTermAt < Factbase::Test
+  def test_refuses_a_negative_position
+    fb = Factbase.new
+    f = fb.insert
+    f.foo = 1
+    f.foo = 2
+    e =
+      assert_raises(StandardError) do
+        fb.query('(eq 2 (at -1 foo))').each.to_a
+      end
+    assert_includes(e.message, 'A non-negative position is expected, but -1 provided', e.message)
+  end
+end
