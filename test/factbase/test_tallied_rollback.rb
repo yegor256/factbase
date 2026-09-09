e295ba8eba98fd0f1470318e9da22ea8c43e76ad
devil <europedreadlydevil@gmail.com>
2026-09-09 09:43:22 +0000

    #833: test that a thrown rollback discards the facts too

---
+++ test/factbase/test_tallied_rollback.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
+require_relative '../test__helper'
+require_relative '../../lib/factbase'
+require_relative '../../lib/factbase/tallied'
+
+# Test.
+# Author:: Yegor Bugayenko (yegor256@gmail.com)
+# Copyright:: Copyright (c) 2024-2026 Yegor Bugayenko
+# License:: MIT
+class TestTalliedRollback < Factbase::Test
+  def test_forgets_facts_inserted_before_rollback
+    fb = Factbase::Tallied.new(Factbase.new)
+    fb.insert.foo = 1
+    fb.txn do |t|
+      t.insert.bar = 2
+      throw :rollback
+    end
+    assert_equal(1, fb.size)
+  end
+end
