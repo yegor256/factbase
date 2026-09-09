1320b72874de69a2967f22073a53859302b90ce8
devil <europedreadlydevil@gmail.com>
2026-09-09 10:01:54 +0000

    #838: pick the facts first, delete them afterwards

---
+++ test/factbase/test_query_delete_atomic.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
+require_relative '../test__helper'
+require_relative '../../lib/factbase'
+require_relative '../../lib/factbase/impatient'
+
+# Test.
+# Author:: Yegor Bugayenko (yegor256@gmail.com)
+# Copyright:: Copyright (c) 2024-2026 Yegor Bugayenko
+# License:: MIT
+class TestQueryDeleteAtomic < Factbase::Test
+  def test_deletes_nothing_when_it_times_out
+    fb = Factbase.new
+    20_000.times do |i|
+      f = fb.insert
+      f.foo = i
+      f.bar = "x#{i}"
+    end
+    before = fb.size
+    imp = Factbase::Impatient.new(fb, timeout: 0.05)
+    assert_raises(StandardError) do
+      imp.query('(and (exists foo) (matches bar "^x[0-9]*$"))').delete!
+    end
+    assert_equal(before, fb.size)
+  end
+end
