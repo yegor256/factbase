c7e123cb55732885da5dc7f76b7438661908c6b8
devil <europedreadlydevil@gmail.com>
2026-09-16 11:14:01 +0000

    #888: keep rubocop happy

--- test/factbase/terms/test_size_subterm.rb
+++ test/factbase/terms/test_size_subterm.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
+require_relative '../../../lib/factbase'
 # SPDX-FileCopyrightText: Copyright (c) 2024-2026 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
 require_relative '../../test__helper'
-require_relative '../../../lib/factbase'
 
 # Test of the terms that inspect a value, over a nested term.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
