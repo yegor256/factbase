84ae1d6d068b3ca6d8cd2e5857be9b8daa0d123e
devil <europedreadlydevil@gmail.com>
2026-09-16 11:13:37 +0000

    #896: keep rubocop happy

--- test/factbase/cached/test_cached_fact_args.rb
+++ test/factbase/cached/test_cached_fact_args.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
+require_relative '../../../lib/factbase'
+require_relative '../../../lib/factbase/cached/cached_fact'
 # SPDX-FileCopyrightText: Copyright (c) 2024-2026 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
 require_relative '../../test__helper'
-require_relative '../../../lib/factbase'
-require_relative '../../../lib/factbase/cached/cached_fact'
 
 # Test of the arguments CachedFact takes.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
