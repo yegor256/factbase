cbbb181d382f68b3c24c44b1778c6ec9daf0e4bc
devil <europedreadlydevil@gmail.com>
2026-09-17 07:01:10 +0000

    #953: check a rule against the facts the factbase holds

---
+++ test/factbase/test_rules_agg.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
+require_relative '../test__helper'
+require_relative '../../lib/factbase'
+require_relative '../../lib/factbase/rules'
+
+# Factbase test.
+# Author:: Yegor Bugayenko (yegor256@gmail.com)
+# Copyright:: Copyright (c) 2024-2026 Yegor Bugayenko
+# License:: MIT
+class TestRulesAgg < Factbase::Test
+  def test_sees_the_facts_of_the_factbase
+    origin = Factbase.new
+    origin.insert.foo = 1
+    origin.insert.foo = 2
+    fb = Factbase::Rules.new(origin, '(gt (agg (always) (count)) 0)')
+    fb.query('(exists foo)').each { |f| f.bar = 9 }
+    assert_equal(2, origin.query('(exists bar)').each.to_a.size)
+  end
+end
