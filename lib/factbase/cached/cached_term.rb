77d7fe7bf88f8f9acde2e7359b9c2343aed15661
devil <europedreadlydevil@gmail.com>
2026-09-17 06:39:57 +0000

    #954: do not cache a term that reads the environment

--- lib/factbase/cached/cached_term.rb
+++ lib/factbase/cached/cached_term.rb
@@ -18,6 +18,7 @@ module Factbase::CachedTerm
   def evaluate(fact, maps, fb)
     return super unless static? && !abstract?
     return super if %i[head unique].include?(@op)
+    return super if env?
     key = [maps.object_id, to_s]
     @cache[key] = super if @cache[key].nil?
     @cache[key]
--- lib/factbase/term.rb
+++ lib/factbase/term.rb
@@ -254,6 +254,17 @@ class Factbase::Term < Factbase::TermBase
     false
   end
 
+  # Does it read the environment (+env+), here or deeper inside?
+  #
+  # An +env+ term answers from outside the factbase, so its result may
+  # change while the factbase stays untouched.
+  #
+  # @return [Boolean] TRUE if +env+ is in there
+  def env?
+    return true if @op == :env
+    @operands.any? { |o| o.is_a?(Factbase::Term) && o.env? }
+  end
+
   def at(fact, maps, fb)
     assert_args(2)
     i = _values(0, fact, maps, fb)
