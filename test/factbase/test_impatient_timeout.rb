8c0a4855dc703401a073997726bb3c87c552b0a2
devil <europedreadlydevil@gmail.com>
2026-09-17 06:36:47 +0000

    #957: refuse a non-positive timeout in Factbase::Impatient

---
+++ test/factbase/test_impatient_timeout.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
+require_relative '../test__helper'
+require_relative '../../lib/factbase'
+require_relative '../../lib/factbase/impatient'
+
+# Factbase test.
+# Author:: Yegor Bugayenko (yegor256@gmail.com)
+# Copyright:: Copyright (c) 2024-2026 Yegor Bugayenko
+# License:: MIT
+class TestImpatientTimeout < Factbase::Test
+  def test_refuses_non_positive_timeout
+    [0, -1, 0.0].each do |t|
+      assert_raises(ArgumentError, t.to_s) { Factbase::Impatient.new(Factbase.new, timeout: t) }
+    end
+  end
+end
