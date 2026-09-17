34ce791b67fe00c97927286fab8729177afee46a
devil <europedreadlydevil@gmail.com>
2026-09-17 07:39:09 +0000

    #948: satisfy rubocop

--- test/factbase/indexed/test_indexed_or_duplicates.rb
+++ test/factbase/indexed/test_indexed_or_duplicates.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
+require_relative '../../../lib/factbase'
+require_relative '../../../lib/factbase/indexed/indexed_factbase'
 # SPDX-FileCopyrightText: Copyright (c) 2024-2026 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
 require_relative '../../test__helper'
-require_relative '../../../lib/factbase'
-require_relative '../../../lib/factbase/indexed/indexed_factbase'
 
 # Factbase test.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
@@ -17,8 +17,7 @@ class TestIndexedOrDuplicates < Factbase::Test
     origin.insert.a = 1
     origin.insert.a = 1
     100.times { origin.insert.a = 5 }
-    fb = Factbase::IndexedFactbase.new(origin)
     query = '(or (eq a 1) (eq a 99))'
-    assert_equal(origin.query(query).each.to_a.size, fb.query(query).each.to_a.size)
+    assert_equal(origin.query(query).each.to_a.size, Factbase::IndexedFactbase.new(origin).query(query).each.to_a.size)
   end
 end
