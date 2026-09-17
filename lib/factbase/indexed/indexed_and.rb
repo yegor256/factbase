ad0803a0a67c7d09cf43c1be268c02d9f89a7a36
devil <europedreadlydevil@gmail.com>
2026-09-17 07:39:26 +0000

    #951: satisfy rubocop

--- lib/factbase/indexed/indexed_and.rb
+++ lib/factbase/indexed/indexed_and.rb
@@ -42,7 +42,7 @@ class Factbase::IndexedAnd
           end
         end
       )
-      hits = tuples.flat_map { |t| entry[:index][t] || [] }.map(&:object_id).to_set
+      hits = tuples.flat_map { |t| entry[:index][t] || [] }.to_set(&:object_id)
       j = maps_array.select { |m| hits.include?(m.object_id) }
       r = maps.respond_to?(:repack) ? maps.repack(j) : j
     else
