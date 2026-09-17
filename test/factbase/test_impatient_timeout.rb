0f4d64eb00a4fe8997ce9acce5829f59ef93a247
devil <europedreadlydevil@gmail.com>
2026-09-17 07:39:58 +0000

    #957: satisfy rubocop

--- test/factbase/test_impatient_timeout.rb
+++ test/factbase/test_impatient_timeout.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
+require_relative '../../lib/factbase'
+require_relative '../../lib/factbase/impatient'
 # SPDX-FileCopyrightText: Copyright (c) 2024-2026 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
 require_relative '../test__helper'
-require_relative '../../lib/factbase'
-require_relative '../../lib/factbase/impatient'
 
 # Factbase test.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
