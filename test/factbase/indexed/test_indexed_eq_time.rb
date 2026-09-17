571e335f8b41e3ddb0e2f7a145b6667ba57f0817
devil <europedreadlydevil@gmail.com>
2026-09-17 06:28:28 +0000

    #945: test that a time literal is found under the index

---
+++ test/factbase/indexed/test_indexed_eq_time.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
+require_relative '../../test__helper'
+require_relative '../../../lib/factbase'
+require_relative '../../../lib/factbase/indexed/indexed_factbase'
+
+# Factbase test.
+# Author:: Yegor Bugayenko (yegor256@gmail.com)
+# Copyright:: Copyright (c) 2024-2026 Yegor Bugayenko
+# License:: MIT
+class TestIndexedEqTime < Factbase::Test
+  def test_finds_facts_by_time
+    when_ = Time.parse('2024-03-01T00:00:00Z')
+    origin = Factbase.new
+    origin.insert.when = when_
+    origin.insert.when = when_
+    origin.insert.when = Time.parse('2025-01-01T00:00:00Z')
+    query = "(eq when #{when_.utc.iso8601})"
+    fb = Factbase::IndexedFactbase.new(origin)
+    assert_equal(origin.query(query).each.to_a.size, fb.query(query).each.to_a.size)
+  end
+end
