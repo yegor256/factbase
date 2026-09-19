f48994279f5cebb766bde05eeb68141f79b16d93
devil <europedreadlydevil@gmail.com>
2026-09-19 08:09:20 +0000

    #1000: test that a slow reader does not stop a writer

--- test/factbase/sync/test_sync_query.rb
+++ test/factbase/sync/test_sync_query.rb
@@ -5,6 +5,7 @@ require_relative '../../../lib/factbase/sync/sync_factbase'
 # SPDX-FileCopyrightText: Copyright (c) 2024-2026 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
+require 'benchmark'
 require_relative '../../test__helper'
 
 # Query test.
@@ -12,6 +13,14 @@ require_relative '../../test__helper'
 # Copyright:: Copyright (c) 2024-2026 Yegor Bugayenko
 # License:: MIT
 class TestSyncQuery < Factbase::Test
+  def test_does_not_hold_the_monitor_while_the_block_runs
+    fb = Factbase::SyncFactbase.new(Factbase.new)
+    3.times { |i| fb.insert.foo = i }
+    Thread.new { fb.query('(always)').each { sleep(0.1) } }
+    sleep(0.05)
+    assert_operator(Benchmark.realtime { fb.insert.bar = 1 }, :<, 0.2)
+  end
+
   def test_queries_many_times
     fb = Factbase::SyncFactbase.new(Factbase.new)
     total = 5
